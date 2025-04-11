2c373f34150f4eba2de4c8f12878a42ece612659
Veniamin <ilkovveniamin@gmail.com>
2025-04-11 12:57:18 +0500

    feat: create entity's

---
+++ src/main/java/cushen_group/veniamin/polyclinic/entity/Discharge.java
@@ -0,0 +1,52 @@
+package cushen_group.veniamin.polyclinic.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "discharges")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Discharge {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate date;
+
+    @Lob
+    @Column(length = 5000)
+    private String text;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_id", nullable = false)
+    private Doctor doctor;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id", nullable = false)
+    private Patient patient;
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Discharge)) {
+            return false;
+        }
+
+        Discharge otherDischarge = (Discharge) obj;
+        return this.getId().equals(otherDischarge.getId());
+    }
+
+}
