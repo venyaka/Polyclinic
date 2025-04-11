70051b2753a0c081aa09fa5cb83b700736fdf916
Veniamin <ilkovveniamin@gmail.com>
2025-04-11 09:23:19 +0500

    init: setup project

---
+++ src/main/java/cushen_group/veniamin/polyclinic/PolyclinicApplication.java
@@ -0,0 +1,13 @@
+package cushen_group.veniamin.polyclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolyclinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cushen_group/veniamin/polyclinic/PolyclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package cushen_group.veniamin.polyclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
