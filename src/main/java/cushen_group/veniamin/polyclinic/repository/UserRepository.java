3f643b1476deaf989d964a480d0f713c3b472ba8
Veniamin <ilkovveniamin@gmail.com>
2025-04-12 21:34:39 +0500

    init: create user repository

---
+++ src/main/java/cushen_group/veniamin/polyclinic/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package cushen_group.veniamin.polyclinic.repository;
+
+import cushen_group.veniamin.polyclinic.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
+    Optional<User> findByEmail(String email);
+
+}
