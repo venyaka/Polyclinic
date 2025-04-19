222bf9e65219dab12b44a5f049e07e540dd76bc8
Veniamin <ilkovveniamin@gmail.com>
2025-04-19 21:43:31 +0500

    feat: update role for spring security

--- src/main/java/cushen_group/veniamin/polyclinic/entity/Role.java
+++ src/main/java/cushen_group/veniamin/polyclinic/entity/Role.java
@@ -4,7 +4,7 @@ import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
 
-    PATIENT, DOCTOR, ADMIN;
+    ROLE_PATIENT, ROLE_DOCTOR, ROLE_ADMIN;
 
     @Override
     public String getAuthority() {
